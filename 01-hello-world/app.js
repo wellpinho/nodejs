7b995568f86961d73aab25187ef17d901d6c3dc8
wellpinho <wellpinho@outlook.com>
2022-07-20 11:45:49 -0300

    🔨 REFACTOR: less code

--- 01-hello-world/app.js
+++ 01-hello-world/app.js
@@ -2,12 +2,8 @@ console.log("Start program");
 
 setTimeout(() => console.log("First timeout"), 3000);
 
-setTimeout(() => {
-  console.log("Second timeout");
-}, 0);
+setTimeout(() => console.log("Second timeout"), 0);
 
-setTimeout(() => {
-  console.log("Third timeout");
-}, 1);
+setTimeout(() => console.log("Third timeout"), 1);
 
 console.log("End program");
